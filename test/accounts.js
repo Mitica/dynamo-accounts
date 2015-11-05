0993c91aee88389b382a3d281d2d9f9c57a299e4
Dumitru Cantea <canteadumitru@gmail.com>
2015-11-05 22:38:43 +0200

    testing many apps

--- test/accounts.js
+++ test/accounts.js
@@ -4,7 +4,9 @@ var assert = require('assert');
 var utils = require('../lib/utils');
 var Promise = utils.Promise;
 var Accounts = require('./common/accounts');
+var apiTest = require('./common/api');
 var APP_ID = '131313131313131313131313';
+var APP_ID2 = '131313131313131313131312';
 
 if (!Accounts) {
 	return;
@@ -33,124 +35,7 @@ describe('Accounts', function() {
 		});
 	});
 
-	describe('#api()', function() {
-		var api = Accounts.api(APP_ID);
-
-		describe('#providerLogin()', function() {
-			it('should login and create a new Account and Connection', function() {
-				return api.providerLogin({
-					id: 'id',
-					provider: 'google',
-					emails: [{
-						value: 'cantea@email.com'
-					}],
-					username: 'admin'
-				}, {}).then(function(account) {
-					assert.ok(account);
-					assert.ok('admin', account.username);
-				});
-			});
-
-			it('should login and detect existing Connection', function() {
-				return api.providerLogin({
-					id: 'id',
-					provider: 'google',
-					emails: [{
-						value: 'cantea@email.com'
-					}],
-					username: 'admin'
-				}, {}).then(function(account) {
-					assert.ok(account);
-					assert.ok('admin', account.username);
-				});
-			});
-
-			it('should fail adding new Connection to one Account for one provider...', function() {
-				return api.providerLogin({
-						id: 'id2',
-						provider: 'google',
-						emails: [{
-							value: 'cantea@email.com'
-						}],
-						username: 'admin'
-					}, {})
-					.catch(function(error) {
-						assert.ok(error);
-					})
-					.then(function(account) {
-						assert.equal(undefined, account);
-					});
-			});
-
-			it('should identify account by Connection email', function() {
-				return api.providerLogin({
-						id: 'id2',
-						provider: 'google',
-						emails: [{
-							value: 'cantea@email.com'
-						}],
-						username: 'admin'
-					}, {})
-					.then(function(account) {
-						assert.ok(account);
-						assert.ok('admin', account.username);
-					});
-			});
-
-			it('should identify account by email for other provider', function() {
-				return api.providerLogin({
-					id: 'id2',
-					provider: 'yahoo',
-					emails: [{
-						value: 'cantea@email.com'
-					}],
-					username: 'admin'
-				}, {}).then(function(account) {
-					assert.ok(account);
-					assert.ok('admin', account.username);
-				});
-			});
-
-			it('should create a new account with a diff username', function() {
-				return api.providerLogin({
-					id: 'id3',
-					provider: 'google',
-					emails: [{
-						value: 'cantea2@email.com'
-					}],
-					username: 'admin'
-				}, {}).then(function(account) {
-					assert.ok(account);
-					assert.notEqual('admin', account.username);
-				});
-			});
-
-			it('should create a new account with username=displayName', function() {
-				return api.providerLogin({
-					id: 'id5',
-					provider: 'google',
-					emails: [{
-						value: 'cantea5@email.com'
-					}],
-					username: 'admin',
-					displayName: 'Ionel'
-				}, {}).then(function(account) {
-					assert.ok(account);
-					assert.equal('Ionel', account.username);
-				});
-			});
-
-		});
-
-		describe('access', function() {
-			describe('#getAccount()', function() {
-				it('should not find account', function() {
-					return api.access.getAccount('id').then(function(account) {
-						assert.equal(undefined, account);
-					});
-				});
-			});
-		});
-	});
+	apiTest(APP_ID);
+	apiTest(APP_ID2);
 
 });
